1881f46b194fcc7b51337a26dc908660a587ebbf
abfleishman <abfleishman@gmail.com>
2017-03-19 20:49:11 -0700

    updates to working copy

--- Scripts/ReSampGPS.R
+++ Scripts/ReSampGPS.R
@@ -6,6 +6,7 @@ rm(list = ls())
 dir='~/Dropbox/RLKI/'
 
 # Load packages
+library(adehabitatLT)
 library(data.table)
 library(dplyr)
 library(stringr)
@@ -22,6 +23,9 @@ for(i in 1:length(list.files("Functions"))) {
 
 # import gps data
 data<-readRDS(paste0(dir,"SGRK_DATA/processedDATA_2015/GPSrlki_raw.rda"))
+
+data<-arrange(data,CaptureID,DateTime)
+
 
 # import handeling data
 Hand<-readRDS(paste0(dir,"SGRK_DATA/processedDATA_2015/CaptureData_2015.rda"))
@@ -51,10 +55,11 @@ unique(data$SiteShort)[!unique(data$SiteShort)%in%CapSitesSel$SiteShort]
 # uses distance (great circle from argosfilter, output in km)
 #data$Dist2Colony<-AddDist2Colony(data = data,CaptureSitesData = CapSitesSel)
 
-data$Dist2Colony<-AddDist2Colony(data = data,CaptureSitesData = CaptureSitesData,SiteName = "SiteShort")
+data$Dist2Colony<-AddDist2Colony(data = data,CaptureSitesData = CapSitesSel,SiteName = "SiteShort")
+
 # adds two columns ColonyMovement and TripNum  to the input, trims trips within a given distance (km) from colony, 
 # but trips must have 4 points to count (can be specified)
-dataT<-MakeTrip(data,ID = "CaptureID",DistCutOff = 0.1,Dist2Colony ="Dist2Colony") 
+dataT<-MakeTrip(data,ID = "CaptureID",DistCutOff = 10,Dist2Colony ="Dist2Colony") 
 
 # Adds in a unique identifier for each trip with year, capture number, and trip number
 A<-str_pad(dataT$CaptureNum, width=3, pad="0")
@@ -72,30 +77,59 @@ dataT$InterPointTime<-InterpointTime(dataT,ID = "CaptureID",DateTime = "DateTime
 #deleates one point not eliminated by speed filter
 dataT <- dataT[-c(31830), ]
 
+datalt<-data
+coordinates(datalt)<-~Longitude+Latitude
+proj4string(datalt)<- CRS('+init=epsg:4326')
+datalt<-as.ltraj(xy = datalt,date = datalt$DateTime,id = datalt$CaptureNum,burst = paste(datalt$CaptureNum,datalt$TripNum))
+
+
 
 dataT<-filter(dataT,CaptureNum==35)
-plot(dataT$Longitude,dataT$Latitude)
+
+# plot(dataT$Longitude[dataT$TripNum==0],dataT$Latitude[dataT$TripNum==0])
+# plot(dataT$Longitude[dataT$TripNum==1],dataT$Latitude[dataT$TripNum==1])
+# plot(dataT$Longitude[dataT$TripNum==10],dataT$Latitude[dataT$TripNum==10])
+# plot(dataT$Longitude[dataT$TripNum==16],dataT$Latitude[dataT$TripNum==16])
+# plot(dataT$Longitude[dataT$TripNum==21],dataT$Latitude[dataT$TripNum==21])
+# plot(dataT$DateTime)
+quartz()
+par(mfrow=c(4,1))
+plot(dataT$DateTime,dataT$Longitude)
+plot(dataT$DateTime,dataT$Latitude)
+plot(dataT$DateTime,dataT$TripNum)
+plot(dataT$DateTime,dataT$Dist2Colony>10)
 head(dataT)
 
 dataT$Bering<-BearingFromPoint(dataIn = dataT,ID = "CaptureID",lat = "Latitude",lon = "Longitude")
 library(circular)
 dataT$Bering<-as.circular(dataT$Bering,type="directions",units = "degrees")
 
-# Azimuth departure -------------------------------------------------------
+# Trip summary stats departure -------------------------------------------------------
 TripTab<-dataT %>% 
   group_by(CaptureID,TripNum) %>% 
   filter(n()>30) %>% 
   summarise(Points=n(),
-            DepartA=mean.circular(Bering[1:5]),
+            DepartA=mean(Bering[1:5]),
             ReturnA=mean(Bering[length(Bering)-5:length(Bering)]),
             Start=min(DateTime),
             End=max(DateTime),
             Max_Dist_Time=DateTime[Dist2Colony==max(Dist2Colony,na.rm=T)],
-            Dist_m=sum(InterPointDist,na.rm=T),
+            Dist_km=sum(InterPointDist,na.rm=T)/1000,
             Max_Dist=max(Dist2Colony,na.rm=T),
             Mean_Speed=mean(Speed,na.rm=T)
             ) %>% 
-  mutate(Duration=difftime(Start,End))
+  mutate(Duration=round(as.numeric(difftime(End,Start,units="hours")),digits=2))
+
+
+
+# Save as kml -------------------------------------------------------------
+
+dataTsp<-as.data.frame(dataT)
+dataTsp$Bering<-as.numeric(dataTsp$Bering)
+coordinates(dataTsp)<-~Longitude+Latitude
+proj4string(dataTsp)<-CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
+
+writeOGR(obj = dataTsp, dsn="dataTsp.kml", layer="dataTsp", driver="KML")
 
 
 
